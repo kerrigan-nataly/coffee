13ce110a1610c56f515c0eaf0680faae460c5afd
Kabachok <94787801+kerrigan-nataly@users.noreply.github.com>
2021-12-07 21:58:14 +0300

    wip

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QTableWidget
 
 
+################### first window #####################
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -17,6 +18,7 @@ class Window(QMainWindow):
         #self.coffee_table.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
         self.coffee_table.setEditTriggers(QTableWidget.NoEditTriggers)
         self.show_coffee()
+        self.add_button.clicked.connect(self.open_AddFormWindow)
         
 
     def show_coffee(self):
@@ -30,6 +32,28 @@ class Window(QMainWindow):
             for j, elem in enumerate(result[i]):
                 self.coffee_table.setItem(i, j, QTableWidgetItem(str(elem)))
 
+    def open_AddFormWindow(self):
+        self.add_form_window = AddFormWindow()
+        self.add_form_window.show()
+        
+
+################### second window #####################
+class AddFormWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.initUi()
+
+    def initUi(self):
+        self.buttonBox.accepted.connect(self.add_coffee)
+        self.buttonBox.rejected.connect(self.close)
+
+    def add_coffee(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+
+        
+        self.close()
 
 
 if __name__ == '__main__':
